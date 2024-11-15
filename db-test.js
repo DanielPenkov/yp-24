c6e01dfd2d4028707b8fa315d391d2c15e9217a5
Daniel Penkov <penkov.daniel@gmail.com>
2024-11-15 17:23:22 +0200

    Test db connection

--- db-test.js
+++ db-test.js
@@ -1,5 +1,11 @@
 const mysql = require('mysql2');
 
+// Ensure that process.env.DATABASE_URL is correctly set
+if (!process.env.DATABASE_URL) {
+    console.error('DATABASE_URL is not defined');
+    process.exit(1);
+}
+
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect((err) => {
